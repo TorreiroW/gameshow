c3e7a44d945785c9790cb07dc649dedcb28ba51d
Wouter van der Toorren <githubwto@toorren.net>
2023-05-23 20:59:28 +0200

    first draft with leds

--- gameShow.ino
+++ gameShow.ino
@@ -20,38 +20,45 @@ long int disablePlayer3 = 0;
 long int disablePlayer4 = 0;
 long int playersTurn = 0;
 long int playerName = "";
+
 void setup() {
   // Zet de seriële communicatie op 9600 baud
   Serial.begin(9600);
 
   // Configureer de ontvanger
   mySwitch.enableReceive(0);
-}
+  // Definieer groene led
+    pinMode(13, OUTPUT);  // Stel de ingebouwde LED-pin (pin 13) in als uitgang
+    pinMode(12, OUTPUT); 
+    digitalWrite(12, LOW);  // Zet pin 8 hoog (5V) om het lampje aan te zetten
 
+}
+  
 long int lookupPlayer(long int buttonID){
   if (buttonID==button1){
     Serial.println("Speler 1");
-    return 'Speler 1';
+    playerName="Speler um";
   }
   if (buttonID==button2){
     Serial.println("Speler 2");
-    return 'Speler 1';
+    playerName="Speler dois";
   }
   if (buttonID==button3){
     Serial.println("Speler 3");
-    return 'Speler 1';
+    playerName="Speler três";
   }
   if (buttonID==button4){
     Serial.println("Speler 4");
-    return 'Speler 4';
+    playerName="Speler quatro";
   }
-  return 'Onbekende speler';
+  return playerName;
 }
 
 void loop() {
   // Waiting for RF-signal
   while (mySwitch.available() == false) {
     delay(100); // delay of 100ms to prevent multiple signals
+  
   }
 
   // Process signal received
@@ -62,12 +69,26 @@ void loop() {
 
   if(receivedValue==buttonA){
     Serial.println("Host started the game");
+    digitalWrite(12, LOW);
+    for (int i = 0; i < 10; i++) {
+    // Code to repeat 10 times
+    // Place your desired code here that you want to repeat
+    digitalWrite(13, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    delay(50); // Optional delay between iterations
+    }
+   
     gameStatus = 1;     // game is set
     // reset disable players
     disablePlayer1 = 0;
     disablePlayer2 = 0; 
     disablePlayer3 = 0; 
     disablePlayer4 = 0; 
+        digitalWrite(LED_BUILTIN, HIGH);  // Zet de ingebouwde LED-pin hoog (5V) om het lampje aan te zetten
+  delay(1000);  // Wacht 1 seconde
+  digitalWrite(LED_BUILTIN, LOW);  // Zet de ingebouwde LED-pin laag (0V) om het lampje uit te zetten
+  delay(1000);  // Wacht 1 seconde
   }
 
   if(receivedValue==buttonB && gameStatus==1){
@@ -77,6 +98,8 @@ void loop() {
 
   
   if (gameStatus==2){
+    digitalWrite(12, LOW);  // Zet pin 8 hoog (5V) om het lampje aan te zetten
+    digitalWrite(13, HIGH);  // Zet pin 8 hoog (5V) om het lampje aan te zetten
     Serial.println("Ready to receive buzz in");
 
     // Check for player buzz-in
@@ -101,6 +124,8 @@ void loop() {
         Serial.println("Player 4 can guess");
         gameStatus=3;
       }
+      digitalWrite(13, HIGH);
+      digitalWrite(12, HIGH);
     }
 
   }
@@ -126,14 +151,40 @@ void loop() {
     }
   }
     if (receivedValue==buttonD && gameStatus==3){
+      for (int i = 0; i < 20; i++) {
+    // Code to repeat 10 times
+    // Place your desired code here that you want to repeat
+    digitalWrite(13, HIGH);
+    digitalWrite(12, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    delay(50); // Optional delay between iterations
+    }
+
       Serial.print("We've got a winner: ");
-      Serial.println(playersTurn);
+      //Serial.println(playersTurn);
       long int winnersName = lookupPlayer(playersTurn);
+      Serial.println("And the winner is:");
+      Serial.print(winnersName);
       gameStatus=0;
     }
   if(receivedValue==buttonC && gameStatus==3){
     Serial.print("Answer wrong of player:");
     Serial.println(playersTurn);
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);  // Zet pin 8 hoog (5V) om het lampje aan te zetten
+    delay(5000);
+    for (int i = 0; i < 10; i++) {
+    // Code to repeat 10 times
+    // Place your desired code here that you want to repeat
+    digitalWrite(12, LOW);
+    delay(50);
+    digitalWrite(12, HIGH);
+    delay(50); // Optional delay between iterations
+    digitalWrite(13, HIGH);
+    }
+    
     if (playersTurn==button1){disablePlayer1=1;}
     if (playersTurn==button2){disablePlayer2=1;}
     if (playersTurn==button3){disablePlayer3=1;}
